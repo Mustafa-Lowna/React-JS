2fe699161c5faf03855f43a995912cf3feacbe0c
Mustafa-Lowna <mustafalowna321@gmail.com>
2025-10-29 19:41:54 +0530

    React JS

---
+++ My-App/src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div>
+      <h1>Welcome to the world of React</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque a
+        deleniti illo pariatur voluptatibus nulla velit cum eum modi sit? Quasi
+        aliquam a omnis inventore, vitae vero iste reprehenderit eum? Lorem,
+        ipsum dolor sit amet consectetur adipisicing elit. Doloremque a deleniti
+        illo pariatur voluptatibus nulla velit cum eum modi sit? Quasi aliquam a
+        omnis inventore, vitae vero iste reprehenderit eum?
+      </p>
+    </div>
+  );
+}
+export default App;
