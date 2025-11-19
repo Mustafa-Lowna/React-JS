77f4fb5f92b007cc42c39e14207859afffdecd81
Mustafa-Lowna <mustafalowna321@gmail.com>
2025-11-19 19:59:17 +0530

    Fix UserList Component

--- My-App/src/Components/UserList.tsx
+++ My-App/src/Components/UserList.tsx
@@ -20,7 +20,7 @@ function UserList() {
   const fetchUsers = async () => {
     const response = await fetch("https://jsonplaceholder.typicode.com/users");
     const data = await response.json();
-    setUsers(data as User[]);
+    setUsers(data);
   };
 
   return (
@@ -31,9 +31,9 @@ function UserList() {
       </button>
       <ul className="list-group mt-4">
         {users.map((user) => (
-          <li key={user.id} className="list-group-item">
+          <li key={user.id} className="list-group-item mb-2">
             <h4 className="text-warning mb-4">{user.name}</h4>
-            <p className="text-secondary">{user.email}</p>
+            <p className="text-secondary">{user.email}</p>s
             <p className="text-secondary">
               {user.address.street}, {user.address.suite}
             </p>
